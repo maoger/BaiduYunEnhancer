010e5e87b340f2f1f2543493cadbf2915b3c53a8
maoger <maoyantsing@gmail.com>
2016-12-27 20:21:53 +0800

    V 0.3.1

--- BaiduYunEnhancer.user.js
+++ BaiduYunEnhancer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BaiduYunEnhancer
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.3.1
 // @description  通过修改浏览器的操作系统和（或）硬件平台（navigator.platform），破解必须使用百度云软件下载的限制；使用此脚本后，您可以使用浏览器直接下载，或者使用迅雷下载。
 // @author       Maoger
 // @match        https://pan.baidu.com/*
