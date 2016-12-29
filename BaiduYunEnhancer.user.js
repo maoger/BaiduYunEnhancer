76e721bbaae9bba901ef2ffea091d4553397706f
maoger <maoyantsing@gmail.com>
2016-12-29 22:29:37 +0800

    V 0.5.1

--- BaiduYunEnhancer.user.js
+++ BaiduYunEnhancer.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         BaiduYunEnhancer
-// @namespace    https://github.com/maoger/BaiduYunEnhancer
-// @version      0.5
-// @description  通过修改浏览器的操作系统和（或）硬件平台（navigator.platform），破解百度云/百度网盘的下载限制。
+// @namespace    http://tampermonkey.net/
+// @homepage     https://github.com/maoger/BaiduYunEnhancer
+// @version      0.5.1
+// @description  通过修改浏览器的操作系统和（或）硬件平台（navigator.platform），破解 百度云/百度网盘 的下载限制。
 // @author       Maoger
 // @match        https://pan.baidu.com/*
 // @match        http://pan.baidu.com/*
